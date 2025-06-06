e04c7bb589aaa53b48cdd70323d2ea3206f5621c
victordev018 <joawrt8520@gmail.com>
2025-06-06 12:41:33 -0300

    refactor: rename MQ135 functions with consistent prefix and add detailed comments

--- main/src/main.c
+++ main/src/main.c
@@ -17,8 +17,8 @@ void setup() {
     // inicializando o sensor DHT22
     dht11_init();
 
+    // inicializa o sensor de gás MQ135
     mq135_init();
-
     
 }
 
@@ -32,24 +32,31 @@ int main()
 
     // variáveis para armazenar os valores de temperatura e umidade
     int temperature, humidty;
-
     
+    // variáveis para armazenar os resultados em relação a qualidade do ar
+    float raw_value;        // valor bruto do adc
+    float percent_value;    // conversão para porcentagem  
 
     while (true) {
-        float voltage = read_mq135();
-        printf("Qualidade: %s \n",air_quality_category(voltage));
 
+        // fazendo leitura do sensor de gás mq135
+        raw_value = mq135_read_raw();
+        percent_value = mq135_read_percentage(raw_value);
+        char *air_status = mq135_get_category(percent_value);
+        
+        // envia pulso para o dht11 começar a leitura
         dht11_send_pulse_start();
-
-
-        air_quality_category(read_mq135());
 
         // fazendo leitura e exibindo os valores no serial monitor
         if (dht11_get(&temperature, &humidty)) {
-            //printf("Temperature: %d °C - Humidity: %d \n", temperature, humidty);
+            // exibe os dados do sensor dht11
+            printf("Temperature: %d °C - Humidity: %d \n", temperature, humidty);
+
+            // exibe os dados do sensor de gás mq135
+            printf("Qualidade do Ar: %.1f%% - %s\n", percent_value, air_status);
 
         } else {
-            //printf("Failed to read dht11 data\n");
+            printf("Failed to read dht11 data\n");
         }
 
         sleep_ms(1000);
