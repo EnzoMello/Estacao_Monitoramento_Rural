b5bd9a589be99d444a1fdcfd331dc69b9e6734b0
KaioGabriel-the <kkaiogabrielk@gmail.com>
2025-06-05 21:47:05 -0300

    ✨ feat/mq135: fazendo biblioteca do sensor mq135.

---
+++ main/src/utils/mq135/mq135.h
@@ -0,0 +1,11 @@
+#ifdef MQ135_H
+#define MQ135_H
+
+#include "pico/stdlib.h"
+
+float read_voltage(uint16_t raw_adc); // Função para ler a tensão do ADC e retornar em volts
+const char* air_quality_category(float voltage); // Função para categorizar a qualidade do ar com base na tensão
+int read_mq135(); // Função para ler o MQ-135 e a voltagem correspondente
+void mq135_init(); // Função para inicializar o MQ-135
+
+#endif
